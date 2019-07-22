8aa8aee17f5a1106ebe449f827dfff1e646d6fbd
Amelie Certin <amelie.certin@gmail.com>
2019-07-22 09:59:26 +0200

    Hide or display join button

--- app/src/main/java/com/example/blind_test/adapter/GameAdapter.java
+++ app/src/main/java/com/example/blind_test/adapter/GameAdapter.java
@@ -1,18 +1,13 @@
 package com.example.blind_test.adapter;
 
 import android.content.Context;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.TextView;
-import android.support.v4.app.FragmentTransaction;
 
 import com.example.blind_test.R;
-import com.example.blind_test.fragments.Game;
 import com.example.blind_test.model.Lobbies;
 
 import java.util.List;
@@ -35,7 +30,6 @@ public class GameAdapter extends ArrayAdapter<Lobbies> {
             viewHolder = new GameViewHolder();
             viewHolder.title = convertView.findViewById(R.id.title);
             viewHolder.description = convertView.findViewById(R.id.description);
-            viewHolder.join = convertView.findViewById(R.id.join);
             convertView.setTag(viewHolder);
 
         }
@@ -45,15 +39,6 @@ public class GameAdapter extends ArrayAdapter<Lobbies> {
         viewHolder.title.setText(lobby.getTheme_title());
         viewHolder.description.setText(lobby.getTheme_description());
 
-        viewHolder.join.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager manager = ((AppCompatActivity)getContext()).getSupportFragmentManager();
-                FragmentTransaction fr = manager.beginTransaction();
-                fr.replace(R.id.activity_main_frame_layout, new Game());
-                fr.commit();
-            }
-        });
 
         return convertView;
     }
@@ -61,6 +46,5 @@ public class GameAdapter extends ArrayAdapter<Lobbies> {
     private class GameViewHolder{
         public TextView title;
         public TextView description;
-        public Button join;
     }
 }
